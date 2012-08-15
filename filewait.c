c88b093043f17fa0c8592e2521e017d790506636
Anish Tondwalkar <anish@tjhsst.edu>
2012-08-15 13:29:34 -0400

    fixed indentation

--- filewait.c
+++ filewait.c
@@ -26,45 +26,46 @@
 #include <sys/event.h>
 #include <sys/time.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-    /* insist on a single argument -- the file to wait on. */
-    if (argc != 2) {
-        printf("usage: filewait filename\n");
-        exit(1);
-    }
-
-    /* allocate a kqueue */
-    int kq = kqueue();
-    if (kq == -1) {
-	perror("kqueue");
-	exit(1);
-    }
-
-    struct kchanges changes[argv];
-    for(int i=0; i<argc-1;i++){
-	    /* open a read-only file descriptor to the specified file */
-	    int fd = open(argv[i+1], O_RDONLY);
-	    if (fd == -1) {
-		perror("open");
+	/* insist on a single argument -- the file to wait on. */
+	if (argc != 2) {
+		printf("usage: filewait filename\n");
 		exit(1);
-	    }
-
-	    /* define a kevent structure for waiting on file events */
-	    struct kevent *ke = calloc(1, sizeof(struct kevent));
-	    ke->ident = fd;
-	    ke->filter = EVFILT_VNODE;
-	    ke->flags = EV_ADD|EV_ENABLE|EV_ONESHOT;
-	    ke->fflags = NOTE_DELETE|NOTE_WRITE|NOTE_ATTRIB|NOTE_RENAME;
-	    ke->data = 0;
-	    ke->udata = 0;
-	    kchanges[i]=ke;
 	}
 
-    /* wait indefinitely for an event to occur on the file */
-    struct kevent events[1];
-    kevent(kq, kchanges, argc, events, 1, NULL);
+	/* allocate a kqueue */
+	int kq = kqueue();
+	if (kq == -1) {
+		perror("kqueue");
+		exit(1);
+	}
 
-    exit(0);
+	struct kchanges changes[argv];
+	for (int i = 0; i < argc - 1; i++) {
+		/* open a read-only file descriptor to the specified file */
+		int fd = open(argv[i + 1], O_RDONLY);
+		if (fd == -1) {
+			perror("open");
+			exit(1);
+		}
+
+		/* define a kevent structure for waiting on file events */
+		struct kevent *ke = calloc(1, sizeof(struct kevent));
+		ke->ident = fd;
+		ke->filter = EVFILT_VNODE;
+		ke->flags = EV_ADD | EV_ENABLE | EV_ONESHOT;
+		ke->fflags =
+		    NOTE_DELETE | NOTE_WRITE | NOTE_ATTRIB | NOTE_RENAME;
+		ke->data = 0;
+		ke->udata = 0;
+		kchanges[i] = ke;
+	}
+
+	/* wait indefinitely for an event to occur on the file */
+	struct kevent events[1];
+	kevent(kq, kchanges, argc, events, 1, NULL);
+
+	exit(0);
 }
-
